92368798b9c58d95914449935e2d164b7e7e9383
Pierre-Alexandre St-Jean <pastjean@pastjean.local>
2015-11-16 14:32:01 -0500

    Websocket, tests, gradle, ...
    - Added a base websocket echo server
    - Added base test
    - Added gradle wrapper with 2.8 version all for autocompletion
    - Javaslang and some test libs in build.gradle

--- src/main/java/com/github/glo2003/HelloWorld.java
+++ src/main/java/com/github/glo2003/HelloWorld.java
@@ -2,16 +2,17 @@ package com.github.glo2003;
 
 import static spark.Spark.*;
 
+import javaslang.control.Try;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        port(Integer.valueOf(System.getenv("PORT")));
-      
-        get("/hello", (req, res) -> "Hello World");
 
-        exception(NotFoundException.class, (e, req, res) -> {
-            res.status(404);
-            res.body("Resource not found");
-        });
-        
+        Integer portNumber = Try.of(() -> Integer.valueOf(System.getenv("PORT")))
+                                .orElse(1357);
+
+        port(portNumber);
+
+        get("/", (req, res) -> "We're way ahead of ya");
+        webSocket("/websocket", EchoWebSocket.class);
     }
 }
---
+++ src/test/java/com/github/glo2003/HelloWorldTest.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2013 - 2015, Coveo Solutions Inc.
+ */
+package com.github.glo2003;
+
+import static com.google.common.truth.Truth.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+    @Test
+    public void aDumbTest()
+    {
+        assertThat(true).isTrue();
+        assert_().that(true).isTrue();
+    }
+}
