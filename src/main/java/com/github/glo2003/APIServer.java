58ba8e8a78903f156fe111934c7cb807b8d42063
Ian Letourneau <letourneau.ian@gmail.com>
2019-01-18 16:33:04 -0500

    Fix spark version

--- src/main/java/com/github/glo2003/APIServer.java
+++ src/main/java/com/github/glo2003/APIServer.java
@@ -28,11 +28,30 @@ public class APIServer
 
         get("/ping", (req, res) -> "pong");
 
-        options("*", (request, response) -> "");
+        enableCORS("*", "*", "*");
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
         before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Request-Method", "*");
-            response.header("Access-Control-Allow-Headers", "*");
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
             response.type("application/json");
         });
     }
