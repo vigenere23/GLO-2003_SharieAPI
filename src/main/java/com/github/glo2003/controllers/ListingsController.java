fd74b3468444f8c75416de458d56a8a23f12c549
GASTP33 <lolgab1@hotmail.com>
2019-01-31 20:02:07 -0500

    Fixed data return + added headers for post + added exceptions logs

--- src/main/java/com/github/glo2003/controllers/ListingsController.java
+++ src/main/java/com/github/glo2003/controllers/ListingsController.java
@@ -2,32 +2,51 @@ package com.github.glo2003.controllers;
 
 import static com.github.glo2003.APIServer.listingsDAO;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.glo2003.models.Listing;
 
 import spark.Request;
 import spark.Response;
-import spark.Route;
 
 public class ListingsController {
 
-  public static Listing getListing(Request req, Response res) {
+  private static ObjectMapper jsonObjectMapper = new ObjectMapper();
+
+  public static String getListing(Request req, Response res) {
     int id = 0;
     try {
       String stringId = req.params(":id");
       id = Integer.parseInt(stringId);
       
     } catch (Exception e) {
-      //TODO: handle exception
+      System.out.println(String.format("Unable to convert id %s to int", id));
+      return "";
     }
     
     // TODO send listingsDAO.get(id) in a json format with correct flags
-    return listingsDAO.get(id);
+    Listing listing = listingsDAO.get(id);
+
+    if (listing == null) {
+      System.out.println(String.format("Unable to find listing with id %d", id));
+      return "";
+    }
+    
+    try {
+      System.out.println("Sending response...");
+      return jsonObjectMapper.writeValueAsString(listing);
+    }
+    catch (Exception e) {
+      System.out.println("Unable to parse Listing as json");
+      return "";
+    }
   }
 
   /*TODO change to void and use res.send instead?*/
-  public static int addListing(Request req, Response res) {
+  public static String addListing(Request req, Response res) {
     Listing listing = new Listing(/*TODO add post data as a Map or Object or...*/);
-    return listingsDAO.save(listing);
-    // TODO send listingsDAO.save(listing) in a json format with corretc flags
+    int id = listingsDAO.save(listing);
+    res.header("Location", String.format("/listings/%d", id));
+    res.status(201);
+    return "";
   }
 }
