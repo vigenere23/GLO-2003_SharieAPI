238a596f1af444277771cdb74b537aa82691aa71
GASTP33 <gab.stpierre.6@ulaval.ca>
2019-02-14 20:31:54 -0500

    * Added controller method names * Added missing doc * Fixed doc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -2,13 +2,12 @@ define({
   "name": "Listings API",
   "version": "0.2.0",
   "description": "An API for listings",
-  "url": "localhost:9090",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-14T03:29:18.459Z",
+    "time": "2019-02-15T01:30:33.353Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
